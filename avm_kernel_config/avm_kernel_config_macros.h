c7bf65f0a4247a6894b44867198a801dff0abb35
YourFritz <git@yourfritz.de>
2016-10-07 01:02:58 +0200

    change alignments and section names

--- avm_kernel_config/avm_kernel_config_macros.h
+++ avm_kernel_config/avm_kernel_config_macros.h
@@ -2,11 +2,8 @@
 #ifndef _AVM_KERNEL_CONFIG_MACROS_H
 #define _AVM_KERNEL_CONFIG_MACROS_H
 
-#define akc 	10
-#define	sakc	11	
-
 	.macro	AVM_KERNEL_CONFIG_START
-		.data		akc
+		.section	"configarea", "a", %progbits
 .L_avm_kernel_config_first_byte:
 	.endm
 
@@ -15,13 +12,11 @@
 	.endm
 
 	.macro	AVM_KERNEL_CONFIG_PTR
-		.data		akc
 		.int		.L_avm_kernel_config_entries
-		.align		16
+		.align		4
 	.endm
 
 	.macro	AVM_KERNEL_CONFIG_ENTRY tag, label
-		.data		akc
 		.int		\tag
 		.ifeq		\tag
 			.int	0
@@ -31,8 +26,7 @@
 	.endm
 
 	.macro	AVM_VERSION_INFO buildnumber, svnversion, firmwarestring
-		.data		akc
-		.align		8
+		.align		3
 .L_avm_version_info:
 1:
 		.ascii		"\buildnumber"
@@ -47,26 +41,23 @@
 
 	.macro	AVM_MODULE_MEMORY index, module, size
 		.ifeq	\index
-			.data		akc
 			.int		0
 			.int		0
 		.else
-			.data		sakc
+			.pushsection	"configareastrings", "a", %progbits
 .L_avm_module_memory_\index:
 			.asciz		"\module"
-			.align		4
-			.data		akc
+			.align		2
+			.popsection
 			.int		.L_avm_module_memory_\index
 			.int		\size
 		.endif
 	.endm
 
 	.macro	AVM_DEVICE_TREE_BLOB subrevision
-		.data		akc
 	.endm
 
 	.macro	AVM_DEVICE_TREE subrevision, filename
-		.data		akc
 		.int		avm_kernel_config_tags_device_tree_subrev_\subrevision
 		.int		._L_avm_device_tree_\subrevision
 	.endm
