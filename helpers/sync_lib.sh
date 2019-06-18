e98986cf66e01ed68f8c93c35e63d2abd293b456
YourFritz <git@yourfritz.de>
2019-06-18 11:37:48 +0200

    sync_lib.sh: wrongly placed script removed

--- helpers/sync_lib.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-[ "$1" = do ] && n="" || n="n"
-printf "Sync to FB7490 ...\n"
-rsync -a${n}P functions/ /ssh/fb7490/var/media/ftp/root/bin/functions/
-printf "Sync to modfs repository ...\n"
-rsync -a${n}P functions/ ../../modfs/bin/scripts/functions/
-printf "creating archive to deploy ...\n"
-tar -c -v -f shell_lib.tar yf_helpers functions
