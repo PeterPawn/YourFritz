25aa25d6c0ae090f7f49acccf4860026f0c4ced0
YourFritz <git@yourfritz.de>
2019-06-18 11:45:05 +0200

    patch_kernel: add SPDX identifiers

--- patch_kernel/src2patch.sh
+++ patch_kernel/src2patch.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
+# SPDX-License-Identifier: GPL-2.0-or-later
 [ -t 1 ] && exec 1>./900-patchkernel_source
 diff -u /dev/null ./yf_patchkernel.c | sed -e '1s|.*|--- /dev/null|' -e '2s|.*|+++ linux-3.10/drivers/net/yf_patchkernel.c|'
